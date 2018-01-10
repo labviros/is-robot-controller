a042c61cdad888fa2ad1cbc4144984eb79ae6445
felippe <mendonca.felippe@gmail.com>
2018-01-10 17:23:00 -0200

    fence limit speed policy updated. - fence limits modified

--- fence.hpp
+++ fence.hpp
@@ -1,30 +1,34 @@
 #ifndef __FENCE_HPP__
 #define __FENCE_HPP__
 
-#include <is/is.hpp>
 #include <is/msgs/common.pb.h>
 #include <is/msgs/robot.pb.h>
-#include "robot-parameters.pb.h"
 #include <armadillo>
+#include <is/is.hpp>
+#include "robot-parameters.pb.h"
 
 namespace fence {
 
 using namespace is::robot;
 using namespace is::common;
 
+Speed limit_speed(Speed speed, float p, float min, float max) {
+  auto dp = fabs(p - max);
+  auto p_out = p > max || p < min;
+  if (p_out) {
+    is::warn("Robot leaving critical area. Reducing speed.");
+    speed.set_linear(std::max(1.0 - dp / 0.5, 0.0) * speed.linear());
+    speed.set_angular(std::max(1.0 - dp / 0.5, 0.0) * speed.angular());
+  }
+  return speed;
+}
+
 Speed limit_speed(RobotControllerProgress const& status, Fence const& fence) {
   Speed speed;
   if (!status.has_current_pose())
     return speed;
-  auto x = status.current_pose().position().x();
-  auto y = status.current_pose().position().y();
-  auto x_out = x > fence.x_max() || x < fence.x_min();
-  auto y_out = y > fence.y_max() || y < fence.y_min();
-  if (x_out || y_out) {
-    is::warn("Robot leaving critical area. Forcing stop.");
-    return speed;
-  }
-  return status.current_speed();
+  speed = limit_speed(status.current_speed(), status.current_pose().position().x(),  fence.x_min(), fence.x_max());
+  return limit_speed(speed, status.current_pose().position().y(),  fence.y_min(), fence.y_max());
 }
 
 }  // ::fence
